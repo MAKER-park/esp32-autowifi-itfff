3bc08b31ca0809c7b02c388797a5106b539ff14f
pjw <pjw970219@gmail.com>
2019-06-24 23:46:34 +0900

    타이머 인터럽트 + pir
    추후 pir 타이밍 조정 필요

--- esp32 + tab test/tab_test/tab_main.ino
+++ esp32 + tab test/tab_test/tab_main.ino
@@ -1,148 +1,79 @@
-void led(int n, int g, int r, int b){
-  for(int i=0;i<n;i++){
-
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(g,r,b)); // Moderately brigh green/red/blue
- 
-    pixels.show(); // This sends the updated pixel color to the hardware.
-
-    delay(delayval); // Delay for a period of time (in milliseconds).
-
-  }
-  
-}
-/*
-void send_event(const char *event,int temp,int hum,int soil){
-  Serial.print("Connecting to ");
-  Serial.println(IFTTT_Host);
-  
-  WiFiClient client;
-  const int httpPort = 80;
-  if (!client.connect(IFTTT_Host, httpPort)) {
-    Serial.println("Connection failed");
-    return;
-  }
-
-  String url = "/trigger/";
-  url += event;
-  url += "/with/key/";
-  url += IFTTT_KEY;
-  
-  String mJson = String("{\"value1\":\"") +temp+"\"}";
-  
-  Serial.print("Requesting URL: ");
-  Serial.println(url);
-                       
-  client.println(String("POST ") + url + " HTTP/1.1");
-  client.println(String("Host: ") + IFTTT_Host);
-  client.println("Content-Type: application/json");
-  client.print("Content-Length: ");  
-  client.println(mJson.length());
-  client.println();
-  client.println(mJson);
- 
-  while(client.connected()){
-    if(client.available()){
-      String line = client.readStringUntil('\r');
-      Serial.print(line);
-    } else {
-      delay(50);
-    };
-  }
-  Serial.println();
-  Serial.println("closing connection");
-  client.stop();
-}*/
-
-void send_event(float temp,float hum,int soil){
+void send_event(float temp, float hum, int soil) {
   char t[8];
   char h[8];
   char s[8];
-  sprintf(t,"%.2f",temp);
-  sprintf(h,"%.2f",hum);
-  sprintf(s,"%d",soil);
+  sprintf(t, "%.2f", temp);
+  sprintf(h, "%.2f", hum);
+  sprintf(s, "%d", soil);
   Serial.println(t);
   Serial.println(h);
   Serial.println(s);
   IFTTTWebhook wh(IFTTT_API_KEY, IFTTT_EVENT_NAME);
-  wh.trigger(t,h,s);
+  wh.trigger(t, h, s);
 
   //mysql
-    HTTPClient http;
+  HTTPClient http;
 
-    String endWeb ="http://192.168.35.93/lunapot_db_test1.php?temp=";//주소 나중에 바꿀것!
-    endWeb += String(t);
-    endWeb += "&soil=";
-    endWeb += String(s);
-    endWeb += "&hum=";
-    endWeb += String(h);
- 
-    http.begin(endWeb);        // 서버 주소 
-    Serial.println(endWeb);
-    int httpCode = http.GET(); 
+  String endWeb = "http://192.168.35.93/lunapot_db_test1.php?temp="; //주소 나중에 바꿀것!
+  endWeb += String(t);
+  endWeb += "&soil=";
+  endWeb += String(s);
+  endWeb += "&hum=";
+  endWeb += String(h);
 
-    if(httpCode>0){
-        String payload = http.getString();
-        Serial.println(payload);
-    }else{
-        Serial.println("Error on HTTP request");
-    }
-    http.end(); 
-    delay(5000);
+  http.begin(endWeb);        
+  Serial.println(endWeb);
+  int httpCode = http.GET();
+
+  if (httpCode > 0) {
+    String payload = http.getString();
+    Serial.println(payload);
+  } else {
+    Serial.println("Error on HTTP request");
+  }
+  http.end();
+  //pir_led();
+  count = 0;
 }
 
 
-void dht_save(){
+void dht_save() {
   //use the functions which are supplied by library.
-h = dht.readHumidity();
-// Read temperature as Celsius (the default)
-t = dht.readTemperature();
-// Check if any reads failed and exit early (to try again).
-if (isnan(h) || isnan(t)) {
-Serial.println("Failed to read from DHT sensor!");
-return;
-}
-// print the result to Terminal
-Serial.print("Humidity: ");
-Serial.print(h);
-Serial.print(" %\t");
-Serial.print("Temperature: ");
-Serial.print(t);
-Serial.println(" *C ");
-//we delay a little bit for next read
-delay(100);
+  h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  t = dht.readTemperature();
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  /* print the result to Terminal
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" *C ");
+  //we delay a little bit for next read*/
+  delay(100);
 }
 
 
 void loop() {
-/*  
-#define button1 39
-*/
-// temp button 39!
-int s = map(analogRead(ADC_PIN), 0, 4096, 100, 0);
-int pir = digitalRead(PIR_PIN);
+  /*
+    #define button1 39
+  */
+  // temp button 39!
+  int pir = digitalRead(PIR_PIN);
+ 
 
-    dht_save();
-    send_event(t,h,s);//ifttt+mysql data 추가
+  dht_save();
+  if(count>=10){
+   send_event(t, h, s); //ifttt+mysql data 추가 
+  }
+  //Serial.println(pir);
+  if ( pir == HIGH) {
+    pir_led();
+  }
 
-
-
-
-if( pir == HIGH){
-  led(map(t,0,45,0,8),20,0,0);
-  delay(1000);
-  led(8,0,0,0);
-  led(map(s,0,100,0,8),0,20,0);
-  delay(1000);
-  led(8,0,0,0);
-  led(map(h,0,100,0,8),0,0,20);
-  delay(1000);
-}
-if( pir == LOW){
-  led(8,0,0,0);
-}
-  /*led(22,0,0,0);
-  delay(1000);
-  led(5,0,0,100);//number, g,r,b
-  delay(1000);*/
 }
--- esp32 + tab test/tab_test/tab_test.ino
+++ esp32 + tab test/tab_test/tab_test.ino
@@ -6,10 +6,10 @@
 #include "IFTTTWebhook.h"
 
 #include <HTTPClient.h>
-#include <string.h> 
+#include <string.h>
 
 #define ADC_PIN         32 //soil
-#define PIR_PIN         21 //PIR
+#define PIR_PIN         27 //PIR
 #define SERVERIP        "192.168.35.93"//ip주소 나중에 외부 ip로 바꿀것
 
 #include "DHT.h"
@@ -20,15 +20,12 @@
 //create an instance of DHT sensor
 DHT dht(DHTPIN, DHTTYPE);
 
-/*2019-04-07 이전것
-#define IFTTT_Host "maker.ifttt.com"
-#define IFTTT_KEY "b1tS_dzyDEabsKWtS4woTJ"
-*/
+
 #define IFTTT_API_KEY "b1tS_dzyDEabsKWtS4woTJ"
 #define IFTTT_EVENT_NAME "luna_pot"
 
 #ifdef __AVR__
-  #include <avr/power.h>
+#include <avr/power.h>
 #endif
 
 // Which pin on the Arduino is connected to the NeoPixels?
@@ -40,52 +37,105 @@ DHT dht(DHTPIN, DHTTYPE);
 
 #define button1 39
 
-int flag=0;
+int flag = 0;
 
-float h =0,t=0;
+float h = 0, t = 0;
+int s = map(analogRead(ADC_PIN), 0, 4096, 100, 0);
+int count = 0;
 
+/* create a hardware timer */
+hw_timer_t * timer = NULL;
 
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
+void IRAM_ATTR onTimer(){
+  count++;
+  Serial.println(count);
+}
+
+//neopixel
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 int delayval = 50; // delay for half a second
 
+void led(int n, int g, int r, int b) {
+  for (int i = 0; i < n; i++) {
+
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(g, r, b)); // Moderately brigh green/red/blue
+
+    pixels.show(); // This sends the updated pixel color to the hardware.
+
+    delay(delayval); // Delay for a period of time (in milliseconds).
+
+  }
+
+}
+
+void pir_led(){
+  Serial.println("motion!");
+  led(8, 0, 0, 0);
+  led(map(t, 0, 45, 0, 8), 20, 0, 0);
+    delay(1000);
+    led(8, 0, 0, 0);
+    led(map(s, 0, 100, 0, 8), 0, 20, 0);
+    delay(1000);
+    led(8, 0, 0, 0);
+    led(map(h, 0, 100, 0, 8), 0, 0, 20);
+    delay(1000);
+    led(8, 0, 0, 0);
+}
+/* wifi state */
+volatile byte wifi_state = LOW;
+
+void wifi(){
+  if(wifi_state == LOW){
+    WiFiManager wifiManager;
+  wifiManager.autoConnect("Luna_pot");
+  Serial.println("connected...yeey :)");
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  wifi_state=HIGH;
+  }
+    
+}
 void setup() {
 
   dht.begin();
 
   pinMode(PIR_PIN, INPUT);
 
+
   pinMode(button1, INPUT);
-  
+
   Serial.begin(115200);
+wifi();
 
-   WiFiManager wifiManager;
 
-
-   wifiManager.autoConnect("Luna_pot");
-
-   Serial.println("connected...yeey :)");
-    // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-
-    /*2019-04-07 new lib webhook
-    IFTTTWebhook wh(IFTTT_API_KEY, IFTTT_EVENT_NAME);
-    wh.trigger();
-    wh.trigger("1");
-    wh.trigger("1", "2");
-    wh.trigger("1", "2", "3");
-    */
-    
 #if defined (__AVR_ATtiny85__)
   if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
 #endif
   // End of trinket special code
 
-  //send_event("luna_pot_temp");  
+  //send_event("luna_pot_temp");
 
   pixels.begin(); // This initializes the NeoPixel library.
   pixels.show();
+  //boot
+  led(8, 100, 0, 0);
+  led(8, 0, 100, 0);
+  led(8, 0, 0, 100);
+  led(8, 100, 100, 100);
+  led(8, 0, 0, 0);
+
+  /* Use 1st timer of 4 */
+  /* 1 tick take 1/(80MHZ/80) = 1us so we set divider 80 and count up */
+  timer = timerBegin(0, 80, true);
+   /* Attach onTimer function to our timer */
+  timerAttachInterrupt(timer, &onTimer, true);
+  /* Set alarm to call onTimer function every second 1 tick is 1us
+  => 1 second is 1000000us */
+  /* Repeat the alarm (third parameter) */
+  timerAlarmWrite(timer, 1000000, true);
+  /* Start an alarm */
+  timerAlarmEnable(timer);
+  Serial.println("start timer");
 
 }
